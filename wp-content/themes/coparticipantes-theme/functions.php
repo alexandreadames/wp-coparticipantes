fe4d0c8dfe843a83621a8738668f14b86d3dd049
Alexandre <alexandre.adames@gmail.com>
2018-07-06 21:35:47 -0300

    Subscription custom fields event_id and user_id

--- wp-content/themes/coparticipantes-theme/functions.php
+++ wp-content/themes/coparticipantes-theme/functions.php
@@ -215,6 +215,9 @@ function slb_get_acf_key( $field_name ) {
 
 /*=====CUSTOM FIELDS========*/
 
+/*-----------------------------------
+Subscription Custom Fields
+/*-----------------------------------*/
 if(function_exists("register_field_group"))
 {
   register_field_group(array (
